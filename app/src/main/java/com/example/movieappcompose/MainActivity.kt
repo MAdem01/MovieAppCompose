1685c10c1e74045afce535eb697282372221e243
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-02 14:40:32 +0200

    add MovieRow composable with Card with a image and name in it

--- app/src/main/java/com/example/movieappcompose/MainActivity.kt
+++ app/src/main/java/com/example/movieappcompose/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.movieappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,8 +28,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.movieappcompose.ui.theme.MovieAppComposeTheme
@@ -55,14 +69,39 @@ fun MyApp(content: @Composable (PaddingValues) -> Unit){
 
 @Composable
 fun MainContent(movieList: List<String> = listOf(
-    "Avatar", "300", "Harry Potter", "Life"
+    "Avatar", "300", "Harry Potter", "Life", "Fight Club", "Superbad"
 )){
     Column(modifier = Modifier
         .padding(12.dp)) {
         LazyColumn {
             items(items = movieList){
-                Text(text = it)
+                MovieRow(it)
+
             }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String){
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 5.dp) {
+                Icon(imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image")
+            }
+            Text(text = movie)
         }
     }
 }
