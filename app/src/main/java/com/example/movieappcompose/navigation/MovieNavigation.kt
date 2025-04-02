e69c591de4375c05032c890178559f00d3c1980b
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-02 15:55:07 +0200

    add HomeScreen to navHost

--- app/src/main/java/com/example/movieappcompose/navigation/MovieNavigation.kt
+++ app/src/main/java/com/example/movieappcompose/navigation/MovieNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.movieappcompose.screens.home.HomeScreen
 
 
 @Composable
@@ -13,7 +14,7 @@ fun MovieNavigation() {
         navController = navController,
         startDestination = MovieScreens.HomeScreen.name) {
         composable(MovieScreens.HomeScreen.name){
-            //HomeScreen()
+            HomeScreen(navController = navController)
         }
     }
 }
