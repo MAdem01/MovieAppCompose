330d1c811cbac8c86838f9f2dbaecd1d71f05272
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-04-02 16:05:43 +0200

    add detailsScreen to MovieNavigation.kt

--- app/src/main/java/com/example/movieappcompose/navigation/MovieNavigation.kt
+++ app/src/main/java/com/example/movieappcompose/navigation/MovieNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.movieappcompose.screens.details.DetailsScreen
 import com.example.movieappcompose.screens.home.HomeScreen
 
 
@@ -13,8 +14,12 @@ fun MovieNavigation() {
     NavHost(
         navController = navController,
         startDestination = MovieScreens.HomeScreen.name) {
+
         composable(MovieScreens.HomeScreen.name){
             HomeScreen(navController = navController)
+        }
+        composable(MovieScreens.DetailScreen.name){
+            DetailsScreen(navController = navController)
         }
     }
 }
